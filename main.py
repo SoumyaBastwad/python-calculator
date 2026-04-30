1133cc84de56f5e613af326099a9678270e85289
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-04-30 23:22:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 operator =input("Enter an operator (+ - * /):")
 num1=float(input("Enter the first number:"))
 num2=float(input("Enter the second number:"))
+
 if operator == "+":
    result= num1+num2
 elif operator == "-":
@@ -11,4 +12,4 @@ elif operator == "*":
    result= num1*num2  
 elif operator == "/":
    result= num1/num2
-   print(result)
+print(result)
